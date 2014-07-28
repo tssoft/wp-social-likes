4643b248afaefa5820c6803b985b67751a7222db
Ivan Lychkov <i.lychkov@ts-soft.ru>
2014-07-28 16:45:10 +0400

    Version updated to 1.10 in the PHP script

--- wp-social-likes.php
+++ wp-social-likes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Social Likes
 Description: Wordpress plugin for Social Likes library by Artem Sapegin (http://sapegin.me/projects/social-likes)
-Version: 1.9
+Version: 1.10
 Author: TS Soft
 Author URI: http://ts-soft.ru/en/
 License: MIT
